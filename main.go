e1fb0a3bb304b49e732af8becce34c9731777061
hurayChan <chan@huray.net>
2023-05-18 06:11:52 +0000

    [ADD] first commit of todo

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"github.com/urfave/negroni"
+)
+
+var rd *render.Render
+
+type Todo struct {
+	ID        int    `json:"id,omitempty"`
+	Name      string `json:"name"`
+	Completed bool   `json:"completed,omitempty"`
+}
+
+var todoMap map[int]Todo
+var lastID int = 0
+
+func MakeWebHandler() http.Handler {
+	todoMap = make(map[int]Todo)
+	mux := mux.NewRouter()
+	mux.Handle("/", http.FileServer(http.Dir("public")))
+	mux.HandleFunc("/todos", GetTodoListHandler).Methods("GET")
+	mux.HandleFunc("/todos", PostTodoHandler).Methods("POST")
+	mux.HandleFunc("/todos/{id:[0-9]+}", RemoveTodoHandler).Methods("DELETE")
+	mux.HandleFunc("/todos/{id:[0-9]+}", UpdateTodoHandler).Methods("PUT")
+	return mux
+}
+
+func GetTodoListHandler(w http.ResponseWriter, r *http.Request) {
+	list := make(Todos, 0)
+	for _, todo := range todoMap {
+		list = append(list, todo)
+	}
+	sort.Sort(list)
+	rd.JSON(w, http.StatusOK, list)
+}
+
+func PostTodoHandler(w http.ResponseWriter, r *http.Request) {
+	var todo Todo
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	lastID++
+	todo.ID = lastID
+	todoMap[lastID] = todo
+	rd.JSON(w, http.StatusCreated, todo)
+}
+
+func RemoveTodoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	if _, ok := todoMap[id]; ok {
+		delete(todoMap, id)
+		rd.JSON(w, http.StatusOK, Success{true})
+	} else {
+		rd.JSON(w, http.StatusNotFound, Success{false})
+	}
+}
+
+func UpdateTodoHandler(w http.ResponseWriter, r *http.Request) {
+	var newTodo Todo
+	err := json.NewDecoder(r.Body).Decode(&newTodo)
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+	// Before
+	/*
+		if todo, ok := todoMap[id]; ok {
+			todo.Name = newTodo.Name
+			todo.Completed = newTodo.Completed
+	*/
+	// After
+	if _, ok := todoMap[id]; ok {
+		newTodo.ID = id
+		todoMap[id] = newTodo
+		rd.JSON(w, http.StatusOK, Success{true})
+	} else {
+		rd.JSON(w, http.StatusBadRequest, Success{false})
+	}
+}
+
+func main() {
+	rd = render.New()
+	m := MakeWebHandler()
+	n := negroni.Classic()
+	n.UseHandler(m)
+
+	log.Println("Started App")
+	port := os.Getenv("PORT")
+	err := http.ListenAndServe(":"+port, n)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ model.go
@@ -0,0 +1,19 @@
+package main
+
+type Todos []Todo
+
+func (t Todos) Len() int {
+	return len(t)
+}
+
+func (t Todos) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t Todos) Less(i, j int) bool {
+	return t[i].ID > t[j].ID
+}
+
+type Success struct {
+	Success bool `json:"success"`
+}
